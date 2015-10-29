297fe81b7f1d2d4cc0edd22deda6f83d94cab19f
dcribeiro2 <dcribeiro2@ws-lab01_114.univel.univel.br>
2015-10-29 19:39:02 -0200

    Mais um pouco

---
+++ src/main/java/br/univel/trabalho_Atacadao/CadastroDeProduto.java
@@ -0,0 +1,58 @@
+package br.univel.trabalho_Atacadao;
+
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+	private int id;
+	private int cdBarras;
+	private double Estoque;
+	private int categoria;
+	private String descricao;
+	private BigDecimal custo;
+	private BigDecimal margemdelucro;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCdBarras() {
+		return cdBarras;
+	}
+	public void setCdBarras(int cdBarras) {
+		this.cdBarras = cdBarras;
+	}
+	public double getEstoque() {
+		return Estoque;
+	}
+	public void setEstoque(double estoque) {
+		Estoque = estoque;
+	}
+	public int getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(int categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getCusto() {
+		return custo;
+	}
+	public void setCusto(BigDecimal custo) {
+		this.custo = custo;
+	}
+	public BigDecimal getMargemdelucro() {
+		return margemdelucro;
+	}
+	public void setMargemdelucro(BigDecimal margemdelucro) {
+		this.margemdelucro = margemdelucro;
+	}
+	
+	
+
+}
---
+++ src/main/java/br/univel/trabalho_Atacadao/TelaCadastroCliente.java
@@ -0,0 +1,127 @@
+package br.univel.trabalho_Atacadao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+
+public class TelaCadastroCliente extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNome;
+	private JTextField textField_1;
+	private JLabel lblTelefone;
+	private JTextField textField_2;
+	private JLabel lblEndereo;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCadastroCliente frame = new TelaCadastroCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaCadastroCliente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblId = new JLabel("Id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 1;
+		gbc_lblId.gridy = 1;
+		contentPane.add(lblId, gbc_lblId);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		lblNome = new JLabel("Nome");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNome.gridx = 1;
+		gbc_lblNome.gridy = 2;
+		contentPane.add(lblNome, gbc_lblNome);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 2;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		lblTelefone = new JLabel("Telefone");
+		GridBagConstraints gbc_lblTelefone = new GridBagConstraints();
+		gbc_lblTelefone.anchor = GridBagConstraints.EAST;
+		gbc_lblTelefone.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTelefone.gridx = 1;
+		gbc_lblTelefone.gridy = 3;
+		contentPane.add(lblTelefone, gbc_lblTelefone);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridy = 3;
+		contentPane.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		lblEndereo = new JLabel("Endere\u00E7o");
+		GridBagConstraints gbc_lblEndereo = new GridBagConstraints();
+		gbc_lblEndereo.anchor = GridBagConstraints.EAST;
+		gbc_lblEndereo.insets = new Insets(0, 0, 0, 5);
+		gbc_lblEndereo.gridx = 1;
+		gbc_lblEndereo.gridy = 4;
+		contentPane.add(lblEndereo, gbc_lblEndereo);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 0, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 2;
+		gbc_textField_3.gridy = 4;
+		contentPane.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+	}
+
+}
